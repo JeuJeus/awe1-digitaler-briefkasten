1346c737f9a67deed5cba43262989a0d518d8ab3
Julius Figge <julius@figge.eu>
2020-04-03 22:59:04 +0200

    add simple frontend example

---
+++ src/main/java/de/fhdw/geiletypengmbh/digitalerbriefkasten/controller/HomepageController.java
@@ -0,0 +1,18 @@
+package de.fhdw.geiletypengmbh.digitalerbriefkasten.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomepageController {
+    @Value("${spring.application.name}")
+    String appName;
+
+    @GetMapping("/")
+    public String homePage(Model model){
+        model.addAttribute("appName", appName);
+        return "home";
+    }
+}

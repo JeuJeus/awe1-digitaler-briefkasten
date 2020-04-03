959073440f62a0a8f5faaf99eb73f317f7986f85
Julius Figge <julius@figge.eu>
2020-04-03 22:37:38 +0200

    initial Commit - empty project

---
+++ src/main/java/de/fhdw/geiletypengmbh/digitalerbriefkasten/DigitalerBriefkastenApplication.java
@@ -0,0 +1,14 @@
+package de.fhdw.geiletypengmbh.digitalerbriefkasten;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalerBriefkastenApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(DigitalerBriefkastenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/fhdw/geiletypengmbh/digitalerbriefkasten/DigitalerBriefkastenApplicationTests.java
@@ -0,0 +1,13 @@
+package de.fhdw.geiletypengmbh.digitalerbriefkasten;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalerBriefkastenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

964fa05429f432b3bde3d7c88ba2f067c93ddae7
Julius Figge <julius@figge.eu>
2020-04-09 12:07:41 +0200

    change to correct Repository

--- src/main/java/de/fhdw/geiletypengmbh/digitalerbriefkasten/persistance/repo/RoleRepository.java
+++ src/main/java/de/fhdw/geiletypengmbh/digitalerbriefkasten/persistance/repo/RoleRepository.java
@@ -1,7 +1,8 @@
 package de.fhdw.geiletypengmbh.digitalerbriefkasten.persistance.repo;
 
 import de.fhdw.geiletypengmbh.digitalerbriefkasten.persistance.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface RoleRepository extends CrudRepository<Role, Long> {
+public interface RoleRepository extends JpaRepository<Role, Long> {
 }

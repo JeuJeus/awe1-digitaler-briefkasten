a2ce6551d2665e6ddb0cca741b26e87134ad6057
Julius Figge <julius@figge.eu>
2020-04-09 10:29:50 +0200

    add views and SecurityConfig

--- src/main/java/de/fhdw/geiletypengmbh/digitalerbriefkasten/config/SecurityConfig.java
+++ src/main/java/de/fhdw/geiletypengmbh/digitalerbriefkasten/config/SecurityConfig.java
@@ -1,19 +1,55 @@
 package de.fhdw.geiletypengmbh.digitalerbriefkasten.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Qualifier("userDetailsServiceImpl")
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .anyRequest()
+        //TODO allow list ideas Request for unauthenticated
+        http
+                .authorizeRequests()
+                .antMatchers("/resources/**", "/registration", "/api/ideas").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
                 .permitAll()
-                .and().csrf().disable();
+                .and()
+                .logout()
+                .permitAll();
+    }
+
+    @Bean
+    public AuthenticationManager myAuthenticationManager() throws Exception {
+        return authenticationManager();
+    }
+
+    @Bean
+    public void configureGlobal(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+        authenticationManagerBuilder
+                .userDetailsService(userDetailsService)
+                .passwordEncoder(bCryptPasswordEncoder());
     }
 }

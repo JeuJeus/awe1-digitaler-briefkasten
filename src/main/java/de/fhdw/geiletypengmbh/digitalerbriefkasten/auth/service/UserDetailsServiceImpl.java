1fd2d456e91ea2c7e073bec3aa95ec1a127ee3d7
Julius Figge <julius@figge.eu>
2020-04-09 08:46:14 +0200

    add UserDetailsService

---
+++ src/main/java/de/fhdw/geiletypengmbh/digitalerbriefkasten/auth/service/UserDetailsServiceImpl.java
@@ -0,0 +1,38 @@
+package de.fhdw.geiletypengmbh.digitalerbriefkasten.auth.service;
+
+import de.fhdw.geiletypengmbh.digitalerbriefkasten.persistance.model.Role;
+import de.fhdw.geiletypengmbh.digitalerbriefkasten.persistance.model.User;
+import de.fhdw.geiletypengmbh.digitalerbriefkasten.persistance.repo.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    @Transactional(readOnly = true)
+    public UserDetails loadUserByUsername(String username) {
+        User user = userRepository.findByUsername(username);
+        if (user == null) throw new UsernameNotFoundException(username);
+
+        Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
+        for (Role role : user.getRoles()) {
+            grantedAuthorities.add(new SimpleGrantedAuthority(role.getName()));
+        }
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), grantedAuthorities);
+    }
+
+}
--- src/main/java/de/fhdw/geiletypengmbh/digitalerbriefkasten/persistance/repo/UserRepository.java
+++ src/main/java/de/fhdw/geiletypengmbh/digitalerbriefkasten/persistance/repo/UserRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface UserRepository extends CrudRepository<User, Long> {
-    List<User> findByUsername(String username);
+    User findByUsername(String username);
 }

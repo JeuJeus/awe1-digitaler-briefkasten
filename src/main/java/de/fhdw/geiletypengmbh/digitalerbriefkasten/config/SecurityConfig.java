3914fdaa4a8cee668676d8ceec097e08a39254db
pbrawe <philipproering94@gmail.com>
2020-04-17 11:27:31 +0200

    fixed login, db name&path

--- src/main/java/de/fhdw/geiletypengmbh/digitalerbriefkasten/config/SecurityConfig.java
+++ src/main/java/de/fhdw/geiletypengmbh/digitalerbriefkasten/config/SecurityConfig.java
@@ -39,6 +39,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .formLogin()
                 .loginPage("/login")
+                .defaultSuccessUrl("/welcome", true)
                 .permitAll()
                 .and()
                 .logout()

aac52ae5d1aa37658b8d577df1344d3335187a4d
Julius Figge <julius@figge.eu>
2020-04-21 17:20:29 +0200

    move from idea controller to idea service

--- src/main/java/de/fhdw/geiletypengmbh/digitalerbriefkasten/controller/IdeaController.java
+++ src/main/java/de/fhdw/geiletypengmbh/digitalerbriefkasten/controller/IdeaController.java
@@ -1,74 +1,52 @@
 package de.fhdw.geiletypengmbh.digitalerbriefkasten.controller;
 
-import de.fhdw.geiletypengmbh.digitalerbriefkasten.controller.exceptions.IdeaIdMismatchException;
-import de.fhdw.geiletypengmbh.digitalerbriefkasten.controller.exceptions.IdeaMalformedException;
-import de.fhdw.geiletypengmbh.digitalerbriefkasten.controller.exceptions.IdeaNotFoundException;
+import de.fhdw.geiletypengmbh.digitalerbriefkasten.auth.service.IdeaService;
 import de.fhdw.geiletypengmbh.digitalerbriefkasten.persistance.model.Idea;
-import de.fhdw.geiletypengmbh.digitalerbriefkasten.persistance.repo.IdeaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.persistence.PersistenceException;
-import java.util.Collection;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Set;
 
 @RestController
 @RequestMapping("api/ideas")
 public class IdeaController {
 
     @Autowired
-    private IdeaRepository ideaRepository;
+    private IdeaService ideaService;
 
     @GetMapping
     public Iterable<Idea> findAll() {
-        return ideaRepository.findAll();
+        return ideaService.findAll();
     }
 
     @GetMapping("/title/{ideaTitle}")
     public List<Idea> findByTitle(@PathVariable String ideaTitle) {
-        return ideaRepository.findByTitle(ideaTitle);
+        return ideaService.findByTitle(ideaTitle);
     }
 
     @GetMapping("/{id}")
     public Idea findOne(@PathVariable Long id) {
-        return ideaRepository.findById(id)
-                .orElseThrow(IdeaNotFoundException::new);
+        return ideaService.findById(id);
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Idea create(@RequestBody Idea idea) {
         //TODO when implementing security not anybody should be allowed to create
-        try {
-            return ideaRepository.saveAndFlush(idea);
-        } catch (Exception e) {
-            //TODO refactor thrown Exception not to be as generous
-            throw new IdeaMalformedException(e);
-        }
+        return ideaService.save(idea);
     }
 
     @DeleteMapping("/{id}")
     public void delete(@PathVariable Long id) {
         //TODO when implementing security not anybody should be allowed to delete
-        ideaRepository.findById(id)
-                .orElseThrow(IdeaNotFoundException::new);
-        ideaRepository.deleteById(id);
+        ideaService.delete(id);
     }
 
     @PutMapping("/{id}")
     public Idea updateIdea(@RequestBody Idea idea, @PathVariable Long id) {
         //TODO not any value should be possible to be updated
-        if (idea.getId() != id) {
-            throw new IdeaIdMismatchException();
-        }
-        ideaRepository.findById(id)
-                .orElseThrow(IdeaNotFoundException::new);
-        return ideaRepository.save(idea);
+        return ideaService.updateIdea(idea, id);
     }
 
 }

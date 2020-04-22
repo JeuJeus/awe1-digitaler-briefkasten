162f4ad6dccbef88a8d8e1c40f90196a0f0e32ed
Jonathan Brockhausen <jonathan.brockhausen@edu.fhdw.de>
2020-04-22 16:55:37 +0200

    idee wird jetzt korrekt angelegt und user wird automatisch herangezogen

--- src/main/java/de/fhdw/geiletypengmbh/digitalerbriefkasten/controller/IdeaController.java
+++ src/main/java/de/fhdw/geiletypengmbh/digitalerbriefkasten/controller/IdeaController.java
@@ -1,9 +1,14 @@
 package de.fhdw.geiletypengmbh.digitalerbriefkasten.controller;
 
 import de.fhdw.geiletypengmbh.digitalerbriefkasten.auth.service.IdeaService;
+import de.fhdw.geiletypengmbh.digitalerbriefkasten.auth.service.UserService;
 import de.fhdw.geiletypengmbh.digitalerbriefkasten.persistance.model.Idea;
+import de.fhdw.geiletypengmbh.digitalerbriefkasten.persistance.model.User;
+import de.fhdw.geiletypengmbh.digitalerbriefkasten.persistance.repo.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,6 +19,9 @@ public class IdeaController {
 
     @Autowired
     private IdeaService ideaService;
+
+    @Autowired
+    private UserService userService;
 
     @GetMapping
     public Iterable<Idea> findAll() {
@@ -30,7 +38,7 @@ public class IdeaController {
         return ideaService.findById(id);
     }
 
-    @PostMapping
+    @PostMapping(consumes = "application/json")
     @ResponseStatus(HttpStatus.CREATED)
     public Idea create(@RequestBody Idea idea) {
         //TODO when implementing security not anybody should be allowed to create
@@ -49,4 +57,18 @@ public class IdeaController {
         return ideaService.updateIdea(idea, id);
     }
 
+    @PostMapping(consumes = "application/x-www-form-urlencoded")
+    public String addIdea(@ModelAttribute Idea idea) {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String username;
+        if (principal instanceof UserDetails) {
+            username = ((UserDetails) principal).getUsername();
+        } else {
+            username = principal.toString();
+        }
+        User creator = userService.findByUsername(username);
+        idea.setCreator(creator);
+        ideaService.save(idea);
+        return "Idee erfolgreich angelegt. Titel: " + idea.getTitle() + ", Beschreibung: " + idea.getDescription();
+    }
 }

bb9b445aaedd09b5c4a9e43c371a808be733d489
Julius Figge <julius@figge.eu>
2020-04-27 20:26:40 +0200

    add Admin User overview

--- src/main/java/de/fhdw/geiletypengmbh/digitalerbriefkasten/persistance/repo/UserRepository.java
+++ src/main/java/de/fhdw/geiletypengmbh/digitalerbriefkasten/persistance/repo/UserRepository.java
@@ -3,6 +3,10 @@ package de.fhdw.geiletypengmbh.digitalerbriefkasten.persistance.repo;
 import de.fhdw.geiletypengmbh.digitalerbriefkasten.persistance.model.account.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
+
+    List<User> findAll();
 }

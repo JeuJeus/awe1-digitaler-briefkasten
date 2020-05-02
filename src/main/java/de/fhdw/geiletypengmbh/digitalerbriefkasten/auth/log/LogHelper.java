f03e67ea624fb49e1ca2b31c0e96062d67bb3868
Julius Figge <julius@figge.eu>
2020-05-02 11:47:04 +0200

    add Listener for Timeout - not working

--- src/main/java/de/fhdw/geiletypengmbh/digitalerbriefkasten/auth/log/LogHelper.java
+++ src/main/java/de/fhdw/geiletypengmbh/digitalerbriefkasten/auth/log/LogHelper.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 
 public class LogHelper {
 
-    public static String getUserIpAddres() {
+    public static String getUserIpAddress() {
         HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes())
                 .getRequest();
         return request.getRemoteAddr();

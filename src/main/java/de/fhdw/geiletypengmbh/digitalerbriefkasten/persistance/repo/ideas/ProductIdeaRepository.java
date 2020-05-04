fbff92f7291a163a997dc0b76ac7775cf8e0fcad
pbrawe <philipproering94@gmail.com>
2020-05-04 11:01:23 +0200

    recfactored InternalIdeaRepository, ProductIdeaRepository

--- src/main/java/de/fhdw/geiletypengmbh/digitalerbriefkasten/persistance/repo/ideas/ProductIdeaRepository.java
+++ src/main/java/de/fhdw/geiletypengmbh/digitalerbriefkasten/persistance/repo/ideas/ProductIdeaRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface ProductIdeaRepository extends JpaRepository<ProductIdea, Long> {
+public interface ProductIdeaRepository extends IdeaRepository<ProductIdea> {
     List<ProductIdea> findByTitle(String title);
 }

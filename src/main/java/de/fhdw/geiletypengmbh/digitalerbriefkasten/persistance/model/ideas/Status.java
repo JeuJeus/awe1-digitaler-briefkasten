7bc596981b8b5f70951c0b4c91db01e53ab528cd
Julius Figge <julius@figge.eu>
2020-05-07 17:53:02 +0200

    update enum to have display names

--- src/main/java/de/fhdw/geiletypengmbh/digitalerbriefkasten/persistance/model/ideas/Status.java
+++ src/main/java/de/fhdw/geiletypengmbh/digitalerbriefkasten/persistance/model/ideas/Status.java
@@ -1,9 +1,19 @@
 package de.fhdw.geiletypengmbh.digitalerbriefkasten.persistance.model.ideas;
 
 public enum Status {
-    NOT_SUBMITTED,
-    PENDING,
-    ACCEPTED,
-    DECLINED,
-    IDEA_STORAGE
+    NOT_SUBMITTED("Nicht eingereicht"),
+    PENDING("auf Bewertung wartend"),
+    ACCEPTED("Angenommen"),
+    DECLINED("Abgelehnt"),
+    IDEA_STORAGE("Im Ideenspeicher");
+
+    public final String displayValue;
+
+    Status(String displayValue) {
+        this.displayValue = displayValue;
+    }
+
+    public String getDisplayValue() {
+        return displayValue;
+    }
 }

7562fa5c2ce56831a230010e34c030f92afac820
pbrawe <philipproering94@gmail.com>
2020-05-15 01:50:58 +0200

    js bs

--- src/main/resources/static/js/prefillSelect2.js
+++ src/main/resources/static/js/prefillSelect2.js
@@ -2,7 +2,7 @@ function prefillSelect2(cssSelector) {
     let selects = document.querySelectorAll(cssSelector);
     selects.forEach((select) => {
         let options = select.options;
-        for (option of options) {
+        for (let option of options) {
             option.selected = true;
         }
         select.dispatchEvent(new Event('change'));

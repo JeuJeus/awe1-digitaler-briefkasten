10d31d7124645ecc6abb87df6a2d3040ac57f6bf
Julius Figge <julius@figge.eu>
2020-05-15 18:35:51 +0200

    add answer

--- src/main/java/de/fhdw/geiletypengmbh/digitalerbriefkasten/service/ideas/IdeaService.java
+++ src/main/java/de/fhdw/geiletypengmbh/digitalerbriefkasten/service/ideas/IdeaService.java
@@ -314,7 +314,8 @@ public class IdeaService {
             User current = userService.getCurrentUser();
             if (toSubmit.getStatus().equals(Status.NOT_SUBMITTED) && toSubmit.getCreator().getId() == current.getId()) {
                 toSubmit.setStatus(Status.PENDING);
-                //TODO / CHECK -> CAN A SPECIALIST CREATE AN IDEA WHICH IS ASSIGNED TO HIMSELF
+                //TODO / CHECK -> CAN A SPECIALIST CREATE AN IDEA WHICH IS ASSIGNED TO HIMSELF -> WOULD LEAD TO NO SEPERATION OF POWER
+                // GROUP ANSWER WAS: Go on a demo, hippie. - There you have separation of powers - some throwing stones, others water
                 toSubmit.setSpecialist(this.getSpecialistOfNewIdea(toSubmit));
                 return save(toSubmit);
             } else {

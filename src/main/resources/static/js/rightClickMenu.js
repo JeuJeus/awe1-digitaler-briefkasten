d04293f31b0d5d72a6de7f8cf612aecf5090896c
pbrawe <philipproering94@gmail.com>
2020-05-16 21:27:30 +0200

    improved rightClickMenu.js, edit and delete only shown on 1st tbl

--- src/main/resources/static/js/rightClickMenu.js
+++ src/main/resources/static/js/rightClickMenu.js
@@ -1,12 +1,12 @@
 function addRightClickMenu(cssSelectorMenu, cssSelectorOnclick) {
-    var menu = document.getElementById(cssSelectorMenu);
-    var elements = document.querySelectorAll(cssSelectorOnclick);
+    let menu = document.querySelector(cssSelectorMenu);
+    let elements = document.querySelectorAll(cssSelectorOnclick);
     for (let elem of elements) {
         if (elem.addEventListener) {
             elem.addEventListener('contextmenu', function (e) {
                 const posX = e.clientX;
                 const posY = e.clientY;
-                showMenu(posX, posY, menu, elem.getAttribute("data"));
+                showMenu(posX, posY, menu, elem.getAttribute("data"), elem);
                 e.preventDefault();
             }, false);
             document.addEventListener('click', function (e) {
@@ -19,7 +19,7 @@ function addRightClickMenu(cssSelectorMenu, cssSelectorOnclick) {
             elem.attachEvent('oncontextmenu', function (e) {
                 const posX = e.clientX;
                 const posY = e.clientY;
-                showMenu(posX, posY, menu, elem.getAttribute("data"));
+                showMenu(posX, posY, menu, elem.getAttribute("data"), elem);
                 e.preventDefault();
             });
             document.attachEvent('onclick', function (e) {
@@ -32,10 +32,33 @@ function addRightClickMenu(cssSelectorMenu, cssSelectorOnclick) {
     }
 }
 
-function showMenu(x, y, menu, data) {
+function showMenu(x, y, menu, data, caller) {
     menu.style.top = y + "px";
     menu.style.left = x + "px";
     menu.style.visibility = "visible";
     menu.style.opacity = "1";
     if (data) menu.setAttribute("data", data);
+    let menuItems = menu.querySelectorAll('.right-click-menu-item');
+    let parentTable = closest(caller, "right-click-table");
+    for (let menuItem of menuItems) {
+        let visibility = JSON.parse(menuItem.getAttribute("data-visibility"));
+        if (visibility && parentTable && visibility.includes(parentTable.id)) {
+            menuItem.style.display = ""; // this resets to default from css
+        } else {
+            menuItem.style.display = "none";
+        }
+    }
+    $(menu).resize();
+}
+
+function closest(el, selectorClass) {
+    var retval = null;
+    while (el) {
+        if (el.className.indexOf(selectorClass) > -1) {
+            retval = el;
+            break
+        }
+        el = el.parentElement;
+    }
+    return retval;
 }
